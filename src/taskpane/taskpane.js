6154ccd38622239a2bf92cfd19c52eeda93f817b
Jason_buaa <waterworth@qq.com>
2024-09-04 16:17:32 +0800

    Simplify the pane layout.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,7 +7,6 @@
 
 // The initialize function must be run each time a new page is loaded
 Office.onReady(() => {
-  document.getElementById("sideload-msg").style.display = "none";
   document.getElementById("app-body").style.display = "flex";
   document.getElementById("run").onclick = run;
 });
